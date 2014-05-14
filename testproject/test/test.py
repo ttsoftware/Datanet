41c6242d3064b38d614af00fa59086c410d2734e
Troels Thomsen <tt.ttsoftware@gmail.com>
2014-05-14 10:55:04 +0200

    test

--- testproject/test/test.py
+++ testproject/test/test.py
@@ -1,3 +1,40 @@
 __author__ = 'troels'
 
-print "hej";
+import socket
+import re
+
+host = '127.0.0.1'
+port = 8080
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((host, port))
+sock.listen(1)
+
+# Loop forever, listening for requests:
+while True:
+    csock, caddr = sock.accept()
+    print "Connection from: " + `caddr`
+    req = csock.recv(1024)  # get the request, 1kB max
+    print req
+    # Look in the first line of the request for a move command
+    # A move command should be e.g. 'http://server/move?a=90'
+    match = re.match('GET /move\?a=(\d+)\sHTTP/1', req)
+    if match:
+        angle = match.group(1)
+        print "ANGLE: " + angle + "\n"
+        csock.sendall("""HTTP/1.0 200 OK
+Content-Type: text/html
+
+<html>
+<head>
+<title>Success</title>
+</head>
+<body>
+Boo!
+</body>
+</html>
+""")
+    else:
+        # If there was no recognised command then return a 404 (page not found)
+        print "Returning 404"
+        csock.sendall("HTTP/1.0 404 Not Found\r\n")
+    csock.close()
